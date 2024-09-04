c3802e3755891d27b84ed700a2b4c779900a66f1
bilecky <bilecky95@gmail.com>
2024-09-04 18:07:15 +0200

    added defaultAvatar if no thumbails

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '9a00de28fde676eb23b5');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'cdbe9f4708d2851b10a6');
--- src/TeamMembers.js
+++ src/TeamMembers.js
@@ -1,35 +1,39 @@
 import React from 'react'
 import { MailAtSign01Icon, SmartPhone04Icon } from 'hugeicons-react'
+import defaultAvatar from './profile-boilerplate.jpg'
 
 const TeamMembers = props => {
 	return (
 		<div className='team-members-grid'>
-			{props.members.map(member => (
-				<div key={member.id} className='member-card'>
-					<div className='member-image-wrapper'>
-						<img
-							src={member.meta.avatar}
-							alt={member.title.rendered}
-							className='member-avatar'
-						/>
-						<div className='member-overlay'>
-							<h2 className='member-name'>{member.title.rendered}</h2>
-							<p className='member-position'>{member.meta.position}</p>
+			{props.members.map(member => {
+				const memberAvatar = member.meta.avatar ? member.meta.avatar : defaultAvatar
+				return (
+					<div key={member.id} className='member-card'>
+						<div className='member-image-wrapper'>
+							<img
+								src={memberAvatar}
+								alt={member.title.rendered}
+								className='member-avatar'
+							/>
+							<div className='member-overlay'>
+								<h2 className='member-name'>{member.title.rendered}</h2>
+								<p className='member-position'>{member.meta.position}</p>
+							</div>
+						</div>
+						<div className='member-details'>
+							<p className='member-bio'>{member.meta.biography}</p>
+							<a href={`mailto:${member.meta.email}`} className='member-contact'>
+								<MailAtSign01Icon size={22} className='icon-email' />
+								<span>{member.meta.email}</span>
+							</a>
+							<a href={`tel:${member.meta.phone}`} className='member-contact'>
+								<SmartPhone04Icon size={22} className='icon-phone' />
+								<span>{member.meta.phone}</span>
+							</a>
 						</div>
 					</div>
-					<div className='member-details'>
-						<p className='member-bio'>{member.meta.biography}</p>
-						<a href={`mailto:${member.meta.email}`} className='member-contact'>
-							<MailAtSign01Icon size={22} className='icon-email' />
-							<span>{member.meta.email}</span>
-						</a>
-						<a href={`tel:${member.meta.phone}`} className='member-contact'>
-							<SmartPhone04Icon size={22} className='icon-phone' />
-							<span>{member.meta.phone}</span>
-						</a>
-					</div>
-				</div>
-			))}
+				)
+			})}
 		</div>
 	)
 }
